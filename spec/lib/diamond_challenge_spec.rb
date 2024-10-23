9beb57ff7ce68a1e160b976a34661d8e33c2caff
VentoPreto <67896322+ventopreto@users.noreply.github.com>
2024-10-22 21:53:41 -0300

    refatora testes

--- spec/lib/diamond_challenge_spec.rb
+++ spec/lib/diamond_challenge_spec.rb
@@ -1,28 +1,38 @@
 require "spec_helper"
 require_relative "../../lib/diamond_challenge"
 
-describe "DiamondChallenge" do
-  it "Recebe uma expressão e retorna o número de diamantes" do
-    expression = "<><><>"
+describe DiamondChallenge do
+  subject { described_class.new }
 
-    expect(DiamondChallenge.new.count_and_remove_diamonds(expression)).to eq 3
-  end
+  describe  "#count_and_remove_diamonds" do
+    context "Quando a expressão contem diamantes" do
+      let(:first_expression) { "<><><>" }
+      let(:second_expression) { "<<.>><<><<<>>.>>" }
+      let(:third_expression) { "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>" }
 
-  it "Recebe uma expressão e retorna o número de diamantes" do
-    expression = "<.><.><.><.><.><.>"
+      it "retorna o número correto de diamantes para a primeira expressão" do
+        expect(subject.count_and_remove_diamonds(first_expression)).to eq 3
+      end
 
-    expect(DiamondChallenge.new.count_and_remove_diamonds(expression)).to eq 6
-  end
+      it "retorna o número correto de diamantes para a segunda expressão" do
+        expect(subject.count_and_remove_diamonds(second_expression)).to eq 7
+      end
 
-  it "Recebe uma expressão e retorna o número de diamantes" do
-    expression = "<<.>><<><<<>>.>>"
+      it "retorna o número correto de diamantes para a terceira expressão" do
+        expect(subject.count_and_remove_diamonds(third_expression)).to eq 11
+      end
+    end
 
-    expect(DiamondChallenge.new.count_and_remove_diamonds(expression)).to eq 7
-  end
+    context "Quando a expressão não contém diamantes" do
+      it "retorna 0" do
+        expect(subject.count_and_remove_diamonds("....")).to eq 0
+      end
+    end
 
-  it "Recebe uma expressão e retorna o número de diamantes" do
-    expression = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
-
-    expect(DiamondChallenge.new.count_and_remove_diamonds(expression)).to eq 11
+    context "Quando a expressão está vazia" do
+      it "retorna 0" do
+        expect(subject.count_and_remove_diamonds("")).to eq 0
+      end
+    end
   end
 end
